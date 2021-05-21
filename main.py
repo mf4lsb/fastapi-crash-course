81f7870c4c0c5f25029a8ca9f89fda36e5bc274a
Naufal Bahri <msn.bahri@gmail.com>
2021-05-21 17:37:02 +0700

    pydantic basemodeL

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Package(BaseModel):
+    name: str
+    number: str
+    description: Optional[str] = None
 
 app = FastAPI()
 
@@ -7,12 +13,16 @@ app = FastAPI()
 async def hello_world():
     return {"hello": "world"}
 
-# Path parameter
-@app.get('/component/{component_id}')
-async def get_component(component_id: int):
-    return {"component_id": component_id}
+# # Path parameter
+# @app.get('/component/{component_id}')
+# async def get_component(component_id: int):
+#     return {"component_id": component_id}
 
-# Query Parameter
-@app.get('/component')
-async def read_component(number:int, text: Optional[str]):
-    return {"number": number, "text": text}
+# # Query Parameter
+# @app.get('/component')
+# async def read_component(number:int, text: Optional[str]):
+#     return {"number": number, "text": text}
+
+@app.post("/package/{priority}")
+async def make_package(priority: int, package: Package, value: bool):
+    return {"priority": priority, **package.dict(), "value": value}
