8513ac80ce5ee7fee061d0fd67a9e679a0ebe504
Naufal Bahri <msn.bahri@gmail.com>
2021-05-21 17:22:35 +0700

    fix pycache

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ app = FastAPI()
 async def hello_world():
     return {"hello": "world"}
 
-# Path parameters
+# Path parameter
 @app.get('/component/{component_id}')
 async def get_component(component_id: int):
     return {"component_id": component_id}
 
-# Query Parameters
+# Query Parameter
 @app.get('/component')
 async def read_component(number:int, text: Optional[str]):
     return {"number": number, "text": text}
